2477443a22f29e8912b7b651cf8a917cd26b483e
Hung Ta <hung.ta@eastagile.com>
2024-04-22 09:52:45 +0700

    [f] OUT-135 - Extract tax status from the query data
    Co-authored-by: DatT<dat.h.tran@eastagile.com>

---
+++ utils/check_tax_status.py
@@ -0,0 +1,11 @@
+from utils.map_keys_to_result import map_keys_to_result
+
+
+def check_tax_status(response):
+    owner_data = map_keys_to_result(response.metadata)
+    if "tax_status" in owner_data:
+        if owner_data["tax_status"] == "OK":
+            return "NO_TAX_DEBT"
+        else:
+            return "TAX_DEBT"
+    return None
