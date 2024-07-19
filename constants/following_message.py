d35c0e2e407e1dfb62a77b2e2ae059506678ff45
Dat Tran <dat.h.tran@eastagile.com>
2024-07-19 11:15:47 +0700

    [c] fix follow up not sending

--- constants/following_message.py
+++ constants/following_message.py
@@ -2,6 +2,6 @@ from enum import Enum
 
 
 class FollowingMessageType(Enum):
-    LAND_BACK = "land_bank"
+    LAND_BANK = "land_bank"
     UNCONFIRMED_TAX_STATUS = "tax_unconfirmed"
     DEFAULT = "match_second_message"
